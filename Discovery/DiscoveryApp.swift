8a139ec36aca7625fe68e5b7373d3297b33cf4e1
Abdulla Haseeb <hasibmlp@gmail.com>
2023-08-20 18:49:03 +0400

    Initial Commit

---
+++ Discovery/DiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoveryApp.swift
+//  Discovery
+//
+//  Created by Abdulla Haseeb on 20/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
